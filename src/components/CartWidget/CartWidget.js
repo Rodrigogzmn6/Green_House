ba4616d25b5350066ccd9718461c0e7e3cbebbad
Rodrigo Guzman <rodrigoguzman6796@gmail.com>
2022-11-22 19:24:45 -0300

    PreEntrega1+Guzman

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './CartWidget.css';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Badge from '@mui/material/Badge';
+
+function CartWidget() {
+    return(
+        <div className="cart-widget">
+            <Badge 
+                badgeContent={2}
+                sx={{
+                    "& .MuiBadge-badge": {
+                        color: '#4E6C50', 
+                        backgroundColor: '#F0EBCE'
+                        }
+                    }}>
+                <ShoppingCartIcon />{" "}
+            </Badge>
+        </div>
+    );
+}
+
+export default CartWidget;
